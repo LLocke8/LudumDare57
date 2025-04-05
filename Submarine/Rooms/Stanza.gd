2230a248e404b92f648ce4db2f586b4f31e1a8f7
Locke8 <68960827+LLocke8@users.noreply.github.com>
2025-04-05 17:16:00 +0200

    Classe base di stanze e stanza base

---
+++ Submarine/Rooms/Stanza.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+class_name Stanza
+
+@export var pressure_resistance : int = 1
+@export var nome : String = "Stanza"
+@export_multiline var descrizione : String = ""
+
+func get_sprite():
+	return $Sprite2D
