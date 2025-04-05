2aee1bb4392fd9c19a228a75bea76d43450ac792
Jeak77 <giacomolanza03@gmail.com>
2025-04-05 17:54:03 +0200

    Creazione stanze
    Create le stanze di base Zavorra, Armatura, Arma, Sonar.

--- Submarine/Rooms/Stanza.gd
+++ Submarine/Rooms/Stanza.gd
@@ -3,6 +3,7 @@ extends Node2D
 class_name Stanza
 
 @export var pressure_resistance : int = 1
+@export var hp : int = 10
 @export var nome : String = "Stanza"
 @export_multiline var descrizione : String = ""
 
